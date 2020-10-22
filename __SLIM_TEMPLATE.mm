524046d1eac7292206be7d83633129b20b85c357
emacle <qkbeyond@gmail.com>
2020-10-22 13:42:48 +0800

    仿XMind 主题

---
+++ __SLIM_TEMPLATE.mm
@@ -0,0 +1,48 @@
+<map version="1.1.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1461834238887" ID="ID_954243233" MODIFIED="1461834254072" TEXT="SLIM_TEMPLATE">
+<node COLOR="#0000ff" CREATED="1457249946072" ID="ID_1935518139" MODIFIED="1459227358227" POSITION="right" TEXT="KNOW-HOW">
+<icon BUILTIN="info"/>
+<node CREATED="1458792971890" ID="ID_1342535390" MODIFIED="1458792999878" TEXT="Relevant external file">
+<icon BUILTIN="attach"/>
+</node>
+<node CREATED="1458015039231" ID="ID_887729186" MODIFIED="1458538252754" TEXT="Useful  info">
+<icon BUILTIN="info"/>
+</node>
+<node CREATED="1458282712894" ID="ID_1428742385" MODIFIED="1458795884664" TEXT="Methods of doing something (not sequentially / hierarchically structured)">
+<icon BUILTIN="password"/>
+</node>
+<node CREATED="1458793340779" ID="ID_1974732948" MODIFIED="1458793688086" TEXT="Process">
+<icon BUILTIN="knotify"/>
+</node>
+</node>
+<node CREATED="1458458211991" ID="ID_1584412108" MODIFIED="1467127980887" POSITION="right" TEXT="ACTIONS">
+<icon BUILTIN="launch"/>
+<node COLOR="#00cc00" CREATED="1457248405553" ID="ID_1095935215" MODIFIED="1461834528444" TEXT="DONE">
+<icon BUILTIN="button_ok"/>
+</node>
+<node COLOR="#3366ff" CREATED="1457248397522" ID="ID_745320606" MODIFIED="1461834522907" TEXT="IN PROGRESS">
+<icon BUILTIN="forward"/>
+</node>
+<node COLOR="#c49a21" CREATED="1458797403007" ID="ID_529866792" MODIFIED="1461029758579" TEXT="BLOCKED">
+<icon BUILTIN="stop"/>
+</node>
+<node COLOR="#cc0033" CREATED="1457248392842" ID="ID_1448350869" MODIFIED="1458536974225" TEXT="TODO">
+<icon BUILTIN="list"/>
+<node CREATED="1468406012861" ID="ID_1959851925" MODIFIED="1468406062281" TEXT="Implement a check for usage reserved words as identifiers">
+<node CREATED="1468406069568" ID="ID_1089686932" LINK="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Lexical_grammar#Keywords" MODIFIED="1468406110474" TEXT="Keywords (MDN)"/>
+</node>
+</node>
+<node COLOR="#663300" CREATED="1458797403007" ID="ID_872820205" MODIFIED="1460609675289" TEXT="WAITING">
+<icon BUILTIN="hourglass"/>
+</node>
+<node COLOR="#999999" CREATED="1457248768407" ID="ID_1490703659" MODIFIED="1458012009294" STYLE="fork" TEXT="MAYBE">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<icon BUILTIN="licq"/>
+</node>
+<node COLOR="#a0522d" CREATED="1458282068494" ID="ID_1956456200" MODIFIED="1458536974229" TEXT="IDEAS">
+<icon BUILTIN="idea"/>
+</node>
+</node>
+</node>
+</map>
